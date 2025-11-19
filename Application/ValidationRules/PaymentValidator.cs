e0036d7a74aab17dc960317be1774ea6a8099926
pepuk24 <pepuk@yahoo.com>
2025-11-20 02:57:05 +0300

    UI arayuzune giriş

--- Application/ValidationRules/PaymentValidator.cs
+++ Application/ValidationRules/PaymentValidator.cs
@@ -1,7 +1,6 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
 using FluentValidation;
-using Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,28 +9,28 @@ using System.Threading.Tasks;
 
 namespace Application.ValidationRules
 {
-    internal class PaymentValidator :AbstractValidator<Payment>
+    internal class PaymentValidator : AbstractValidator<Payment>
     {
         IUnitOfWork unitOfWork;
         public PaymentValidator(IUnitOfWork unitOfWork)
         {
-            
-
-            RuleFor(x => x.Amount).GreaterThan(0)
-                .WithMessage("miktar 0 dan buyuk olmali");
+            RuleFor(x => x.Amount)
+               .GreaterThan(0).WithMessage("Miktar 0'dan Büyük olmalıdır.");
 
             RuleFor(x => x.TransactionId)
-                .NotEmpty().WithMessage("transactionid boş olamaz")
-                .MustAsync(TransactionIdIsUnique).WithMessage("işlem numarası daha once kayıtlı");
-                
+                .NotEmpty().WithMessage("İşlem Numarası Boş Olamaz.")
+                .MustAsync(TransactionIdIsUnique).WithMessage("İşlem Numarası Daha Önce Kayıtlı");
 
-           
-            this.unitOfWork= unitOfWork;
+
+
+
+            this.unitOfWork = unitOfWork;
         }
 
-        async Task<bool> TransactionIdIsUnique(string transactionId,CancellationToken cancellation)
+        async Task<bool> TransactionIdIsUnique(string transactionId, CancellationToken cancellationToken)
         {
-            IEnumerable<Payment> result =await UnitOfWork.Payments.FindAsync(x=>x.TransactionId == transactionId);
+            IEnumerable<Payment> result = await unitOfWork.Payments.FindAsync(x => x.TransactionId == transactionId);
+
             if (result.Count() > 0)
                 return false;
             else
